967bacc8d112b20e8030c321c2d6f1300e47f907
王ing <144081493+Answer5664@users.noreply.github.com>
2026-06-03 16:35:45 +0800

    Add files via upload

---
+++ pokemmo.py
@@ -0,0 +1,27 @@
+companion_1='皮卡丘'
+skill_1='十万伏特'
+skill_2='伏特攻击'
+skill_3='电击'
+skill_4='影子分身'
+companion_2='杰尼龟'
+skill_5='水枪'
+skill_6='攻击提升'
+skill_7='特攻提升'
+skill_8='影子分身'
+print("皮卡丘能使用的技能有")
+print(f"1.{skill_1}",f"2.{skill_2}",sep=' ')
+print(f"3.{skill_3}",f"4.{skill_4}",sep=' ')
+skill=input("请输入你要使用的技能：")
+if skill=='十万伏特':
+    ski_da=120
+    ski_di='电'
+elif skill=='伏特攻击':
+    ski_da=200
+    ski_di='电'
+elif skill=='电击':
+    pass
+elif skill=='影子分身':
+    pass
+elif skill=='水枪':
+    pass
+    
